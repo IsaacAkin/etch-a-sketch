cf8759c07ba9f7c22357cce5ceaa03ed51753633
Halceeyon <IsaacAki247@gmail.com>
2023-12-01 13:23:41 +0000

    Added functionality to the sketchpad buttons
    - selected the sketchpad control buttons and assigned them to variables
    
    - created the function colourMode on line 26. Added a 'click' event listener to the colour button. Selected all items with the 'cell' class and added a 'mouseover' event listener to them that changes their background colour to black
    
    - created the function rainbowMode on line 38. Added a 'click' event listener to the rainbow button. Selected all items with the 'cell' class and added a 'mouseover' event listener to them that changes their background colour to a random RBG value. It does this by calling the generateRandomColour function.
    
    - created the function generateRandomColour on line 50. Created the variable maxValue and assigned the maximum hex value to it. The max value is then multiplied by a random number, and then rounded down to the nearest lowest integer usingn Math.floor. That number it then converted to a hexadecimal string using the toString(16) function. padStart is then used to make sure the number generated it at least 6 digits long. If it isn't then it adds 0's to the left of the number till there are 6 digits. The function then returns the number generated.
    
    - created the function eraseLines on line 58. Added a 'click' event listener to the Eraser button. Selected all items with the 'cell' class and added a 'mouseover' event listener to them that changes their background colour to white, effectively 'erasing' any lines that were previously coloured in
    
    - created the function clearSketchpad on line 70. Added a 'click' event listener to the clear button. Selected all items with the 'cell' class and used foreach to iterate through every cell, changing their background colour to white to effectively clearing the sketchpad

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 "use strict";
 
 const sketchpad = document.querySelector('.sketchpad');
+const colourBtn = document.querySelector('.colour-btn');
+const rainbowBtn = document.querySelector('.rainbow-btn');
+const eraserBtn = document.querySelector('.eraser-btn');
+const clearBtn = document.querySelector('.clear-btn');
 
 // Creates the cells in the sketchpad
 function createCells(cells) {
@@ -11,11 +15,69 @@ function createCells(cells) {
         cell.style.width = `calc(100% / ${cells})`
         sketchpad.appendChild(cell);
 
-        // Changes backgrounsd colour when a cell is hovered over
+        // Changes background colour when a cell is hovered over
         cell.addEventListener('mouseover', () => {
             cell.style.backgroundColor = "black";
         });
     }
 }
 
+// Sets the colour to black
+function colourMode() {
+    colourBtn.addEventListener('click', () => {
+        const cells = document.querySelectorAll('.cell');
+        cells.forEach((cell) => {
+            cell.addEventListener('mouseover', () => {
+                cell.style.backgroundColor = "black";
+            });
+        });
+    });
+}
+
+// Sets the colour to a random colour
+function rainbowMode() {
+    rainbowBtn.addEventListener('click', () => {
+        const cells = document.querySelectorAll('.cell');
+        cells.forEach((cell) => {
+            cell.addEventListener('mouseover', () => {
+                cell.style.backgroundColor = generateRandomColour();
+            });
+        });
+    });
+}
+
+// Generates a random hex colour
+function generateRandomColour() {
+    let maxValue = 0xFFFFFF;
+    let randomNumber = Math.floor(Math.random() * maxValue).toString(16);
+    let randomColour = randomNumber.padStart(6, 0);
+    return `#${randomColour.toUpperCase()}`
+}
+
+// Erases colour when hovered over
+function eraseLines() {
+    eraserBtn.addEventListener('click', () => {
+        const cells = document.querySelectorAll('.cell');
+        cells.forEach((cell) => {
+            cell.addEventListener('mouseover', () => {
+                cell.style.backgroundColor = "white";
+            });    
+        })
+    });
+}
+
+// Resets the sketchpad to default
+function clearSketchpad() {
+    clearBtn.addEventListener('click', () => {
+        const cells = document.querySelectorAll('.cell');
+        cells.forEach((cell) => {
+            cell.style.backgroundColor = "white";
+        });
+    });
+}
+
 createCells(16);
+colourMode();
+rainbowMode();
+eraseLines();
+clearSketchpad();
